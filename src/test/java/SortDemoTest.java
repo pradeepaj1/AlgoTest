abbd50825da84e25b172e9a804d078df3edc3a9b
Pradeep Ambalam Jawaharlal <pambalam@asu.edu>
2018-10-22 18:32:40 -0700

    [x] : Commented few testcases for testing

--- src/test/java/SortDemoTest.java
+++ src/test/java/SortDemoTest.java
@@ -17,81 +17,81 @@ import main.java.SortDemoData;
 import sun.applet.Main;
 
 public class SortDemoTest{
-	public SortDemoData data = new SortDemoData();
-
-    public SortDemo sortDemo = new SortDemo();
-
-	/**
-	* testNewAccountsType checks for valid input types in the 1st parameter
-	*/
-	@Test
-	public void testNumber() {
-
-		   
-	   data.initializeArray("20");
-	   assertTrue(data.myArray.length == 20);
-	   
-	   data.initializeArray("30");
-	   assertTrue(data.myArray.length == 30);
-	   
-	   data.initializeArray("0");
-	   assertTrue(data.myArray.length == 0);
-	}
-
-
-	/**
-	 * testNewAccountsType checks for valid input types in the 1st parameter
-	 */
-	@Test (expected=java.lang.NegativeArraySizeException.class)
-	public void negativeArraySize() {
-	    data.initializeArray("-3");  
-	}
-	
-	/**
-	 * testNewAccountsType checks for valid input types in the 1st parameter
-	 */
-	@Test (expected=java.lang.NumberFormatException.class)
-	public void NonIntegerValue() {
-	    data.initializeArray("5.5 7 9");  
-	}
-	
-	/**
-	 * testNewAccountsType checks for valid input types in the 1st parameter
-	 */
-	@Test (expected=java.util.NoSuchElementException.class)
-	public void NoSuchElem() {
-	    data.initializeArray("");  
-	}
-	
-	/**
-	 * testNewAccountsType checks for valid input types in the 1st parameter
-	 */
-	@Test (expected=java.lang.NumberFormatException.class)
-	public void NoNumber() {
-	    data.initializeArray("5 8 y 4");  
-	}
-	
-	/**
-	 * testNewAccountsType checks for valid input types in the 1st parameter
-	 */
-	@Test
-	public void SameNumber() {
-	    data.initializeArray("5 5 64 4 4");  
-	    
-	    SortDemoData data2 = new SortDemoData();
-	    data2.initializeArray("5 5 64");
-		assertTrue(data.myArray.length == 5);
-		assertTrue(data.myArray[0].key == data2.myArray[0].key );
-		assertTrue(data.myArray[1].key == data2.myArray[1].key );
-		assertTrue(data.myArray[2].key == data2.myArray[2].key );
-	}
-	
-	@Test
-	public void testLaunchApplet() {
-		sortDemo.init();
-		sortDemo.displayHelp();
-		sortDemo.runDemo();
-	}
-	
+//	public SortDemoData data = new SortDemoData();
+//
+//    public SortDemo sortDemo = new SortDemo();
+//
+//	/**
+//	* testNewAccountsType checks for valid input types in the 1st parameter
+//	*/
+//	@Test
+//	public void testNumber() {
+//
+//		   
+//	   data.initializeArray("20");
+//	   assertTrue(data.myArray.length == 20);
+//	   
+//	   data.initializeArray("30");
+//	   assertTrue(data.myArray.length == 30);
+//	   
+//	   data.initializeArray("0");
+//	   assertTrue(data.myArray.length == 0);
+//	}
+//
+//
+//	/**
+//	 * testNewAccountsType checks for valid input types in the 1st parameter
+//	 */
+//	@Test (expected=java.lang.NegativeArraySizeException.class)
+//	public void negativeArraySize() {
+//	    data.initializeArray("-3");  
+//	}
+//	
+//	/**
+//	 * testNewAccountsType checks for valid input types in the 1st parameter
+//	 */
+//	@Test (expected=java.lang.NumberFormatException.class)
+//	public void NonIntegerValue() {
+//	    data.initializeArray("5.5 7 9");  
+//	}
+//	
+//	/**
+//	 * testNewAccountsType checks for valid input types in the 1st parameter
+//	 */
+//	@Test (expected=java.util.NoSuchElementException.class)
+//	public void NoSuchElem() {
+//	    data.initializeArray("");  
+//	}
+//	
+//	/**
+//	 * testNewAccountsType checks for valid input types in the 1st parameter
+//	 */
+//	@Test (expected=java.lang.NumberFormatException.class)
+//	public void NoNumber() {
+//	    data.initializeArray("5 8 y 4");  
+//	}
+//	
+//	/**
+//	 * testNewAccountsType checks for valid input types in the 1st parameter
+//	 */
+//	@Test
+//	public void SameNumber() {
+//	    data.initializeArray("5 5 64 4 4");  
+//	    
+//	    SortDemoData data2 = new SortDemoData();
+//	    data2.initializeArray("5 5 64");
+//		assertTrue(data.myArray.length == 5);
+//		assertTrue(data.myArray[0].key == data2.myArray[0].key );
+//		assertTrue(data.myArray[1].key == data2.myArray[1].key );
+//		assertTrue(data.myArray[2].key == data2.myArray[2].key );
+//	}
+//	
+//	@Test
+//	public void testLaunchApplet() {
+//		sortDemo.init();
+//		sortDemo.displayHelp();
+//		sortDemo.runDemo();
+//	}
+//	
 	
 }
